a81cb0be3cb62e5afc6b07f3a8cf038b7e62967f
Joaquin Cejas <joaquin.cejas.2020@gmail.com>
2023-09-02 11:44:30 -0300

    Added config path

---
+++ config/constantes.php
@@ -0,0 +1,10 @@
+<?php
+//Se definen las constantes de configuracion
+
+define("KEY","");
+define("URL_API","https://www.googleapis.com/youtube/v3/search");
+define("REGION","AR");
+define("MAX_RESULT","10");
+define("TYPE","video");
+define("PART","snippet");
+define("ORDER","relevance");
